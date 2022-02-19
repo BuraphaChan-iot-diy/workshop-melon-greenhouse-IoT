cfcbe1b5e8981586511abd14119ec23bd404dae0
BuraphaChan-iot-diy <99583146+BuraphaChan-iot-diy@users.noreply.github.com>
2022-02-19 23:01:26 +0700

    Update 3.4-Moisture_Soil.ino
    edit

--- module3/3.4-Moisture_Soil.ino
+++ module3/3.4-Moisture_Soil.ino
@@ -1,25 +1,21 @@
-int sensorPin = A0; // select the input pin for the Soil moisture sensor
-int sensorValue = 0; // variable to store the value coming from the sensor
-
 void setup() {
-  // declare the ledPin as an OUTPUT:
   Serial.begin(9600);
 }
 
 void loop() {
-  // read the value from the sensor:
-  sensorValue = analogRead(sensorPin);
-  int humid = map(sensorValue, 0, 1024, 0, 100);
-  humid = 100 - humid;
-  Serial.print("sensor = " );Serial.println(sensorValue);
-  Serial.print("humid = " );Serial.println(humid);
-
-  if(humid <= 30){
-    Serial.println("Dry Soil");
-  }else if(humid > 30 && humid <= 70){
-    Serial.println("Humid Soil");
+  int sensorValue = analogRead(A0);
+  Serial.print("SensorValue:= ");
+  Serial.println(sensorValue);
+  int moisture = map(sensorValue,0,1024,0,100);
+  moisture = 100-moisture;
+  Serial.print("Moisture:= ");
+  Serial.println(moisture);
+  if(moisture<30){
+    Serial.println("Dry");
+  }else if(moisture>=30 && moisture <70){
+    Serial.println("Normal");
   }else{
-    Serial.println("in Water");
+    Serial.println("Wet");
   }
   delay(3000);
 }
